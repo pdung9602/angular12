f8f90afa1106d57166eb95e37f2eab07dae75dbb
phuongbv <phuongbv@bidv.com.vn>
2022-08-31 08:44:11 +0700

    rename project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Anglular12
+# Angular12
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.1.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Anglular12'`, () => {
+  it(`should have as title 'Angular12'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Anglular12');
+    expect(app.title).toEqual('Angular12');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Anglular12 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular12 app is running!');
   });
 });
